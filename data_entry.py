74ddb1dc12ffa3666b60f3fc4291a418ee5d7cf2
Sven <67136317+Sven-Bo@users.noreply.github.com>
2023-05-29 10:54:00 +0200

    Add check to create 'Data_Entry.xlsx' if it doesn't exist
    Add check to create 'Data_Entry.xlsx' if it doesn't exist. The application now initializes an empty DataFrame if 'Data_Entry.xlsx' is not found in the current directory, enabling the handling of new files. The updated record is saved to 'Data_Entry.xlsx', creating the file if necessary.

--- data_entry.py
+++ data_entry.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 import PySimpleGUI as sg
 import pandas as pd
 
@@ -8,7 +7,12 @@ sg.theme('DarkTeal9')
 
 current_dir = Path(__file__).parent if '__file__' in locals() else Path.cwd()
 EXCEL_FILE = current_dir / 'Data_Entry.xlsx'
-df = pd.read_excel(EXCEL_FILE)
+
+# Load the data if the file exists, if not, create a new DataFrame
+if EXCEL_FILE.exists():
+    df = pd.read_excel(EXCEL_FILE)
+else:
+    df = pd.DataFrame()
 
 layout = [
     [sg.Text('Please fill out the following fields:')],
@@ -41,7 +45,7 @@ while True:
     if event == 'Submit':
         new_record = pd.DataFrame(values, index=[0])
         df = pd.concat([df, new_record], ignore_index=True)
-        df.to_excel(EXCEL_FILE, index=False)
+        df.to_excel(EXCEL_FILE, index=False)  # This will create the file if it doesn't exist
         sg.popup('Data saved!')
         clear_input()
 window.close()
